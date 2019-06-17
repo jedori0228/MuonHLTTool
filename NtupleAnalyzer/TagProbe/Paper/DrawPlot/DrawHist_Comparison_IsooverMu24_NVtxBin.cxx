c0eba142090e77205b8861a952c136e53b2c8b36
Jae Sung Kim <jae.sung.kim@cern.ch>
2019-06-17 16:25:53 +0900

    update

--- NtupleAnalyzer/TagProbe/Paper/DrawPlot/DrawHist_Comparison_IsooverMu24_NVtxBin.cxx
+++ NtupleAnalyzer/TagProbe/Paper/DrawPlot/DrawHist_Comparison_IsooverMu24_NVtxBin.cxx
@@ -31,34 +31,37 @@ void DrawHistForEachVariable(TString var, int vtxmin, int vtxmax, Bool_t setZoom
   gSystem->mkdir(outdir, kTRUE);
   canvasRatio->SetOutputDir(outdir);
 
-  canvasRatio->Register(g1, "Run2016H", kBlack);
-  canvasRatio->Register(g2, "Run2018D", kBlue);
+  canvasRatio->Register(g1, "2016 Data", kBlack);
+  canvasRatio->Register(g2, "2018 Data", kBlue);
 
   TString titleX = "";
-  if( var == "Pt" )  titleX = "P_{T}(#mu) [GeV]";
+  if( var == "Pt" )  titleX = "p_{T}(#mu) [GeV]";
   if( var == "Eta" ) titleX = "#eta(#mu)";
   if( var == "Phi" ) titleX = "#phi(#mu)";
-  if( var == "Vtx" ) titleX = "# vtx";
+  if( var == "Vtx" ) titleX = "Number of primary vertices";
 
-  canvasRatio->SetTitle( titleX, "Efficiency", "Run2018D/Run2016H");
+  canvasRatio->SetTitle( titleX, "Isolation/L3 efficiency", "2018/2016");
   canvasRatio->SetLegendPosition( 0.60, 0.32, 0.95, 0.45 );
   // canvasRatio->SetLegendColumn(2);
 
-  if( var == "Pt" && setZoomIn ) canvasRatio->SetRangeX(24, 120);
+  if( var == "Pt" && setZoomIn ) canvasRatio->SetRangeX(24, 500);
+  if( var == "Pt" && !setZoomIn ) canvasRatio->SetRangeX(0, 500);
+  if( var == "Vtx" )              canvasRatio->SetRangeX(2.5, 50.5);
 
   canvasRatio->SetRangeY( 0.8, 1.05 );
-  if( var == "Pt" && !setZoomIn) canvasRatio->SetRangeY( 0, 1.1 );
+  //if( var == "Pt" && !setZoomIn) canvasRatio->SetRangeY( 0, 1.1 );
 
   canvasRatio->SetRangeRatio( 0.85, 1.15 );
 
   canvasRatio->Latex_CMSPre();
+  canvasRatio->RegisterLatex( 0.57, 0.96, "#font[42]{#scale[0.6]{10 fb^{-1} (13 TeV, 2016 and 2018)}}");
 
 
   //canvasRatio->RegisterLatex( 0.60, 0.96, "#font[42]{#scale[0.6]{RunXXX (XX pb^{-1})}}");
-  canvasRatio->RegisterLatex( 0.16, 0.91, "#font[42]{#scale[0.6]{IsoMu24/Mu24}}");
+  //canvasRatio->RegisterLatex( 0.16, 0.91, "#font[42]{#scale[0.6]{IsoMu24/Mu24}}");
   if( var != "Pt" )
-    canvasRatio->RegisterLatex( 0.16, 0.87, "#font[42]{#scale[0.6]{P_{T} > 26 GeV}}");
-    canvasRatio->RegisterLatex( 0.16, 0.83, "#font[42]{#scale[0.6]{"+TString::Itoa(vtxmin,10)+" #leq Nvtx < "+TString::Itoa(vtxmax,10)+"}}");
+    canvasRatio->RegisterLatex( 0.16, 0.91, "#font[42]{#scale[0.6]{P_{T} > 26 GeV}}");
+    canvasRatio->RegisterLatex( 0.16, 0.87, "#font[42]{#scale[0.6]{"+TString::Itoa(vtxmin,10)+" #leq Number of primary vertices < "+TString::Itoa(vtxmax,10)+"}}");
 
   canvasRatio->Draw();
 }
--- NtupleAnalyzer/TagProbe/Paper/DrawPlot/DrawHist_Comparison_L3overL1.cxx
+++ NtupleAnalyzer/TagProbe/Paper/DrawPlot/DrawHist_Comparison_L3overL1.cxx
@@ -3,7 +3,7 @@
 void DrawHistForEachVariable(TString var, Bool_t setZoomIn = kFALSE);
 TGraphAsymmErrors* GetEffGraph( TString fileName, TString var );
 
-void DrawHist_Comparison_L2overL1() {
+void DrawHist_Comparison_L3overL1() {
   DrawHistForEachVariable( "Pt" );
   DrawHistForEachVariable( "Pt", kTRUE );
   DrawHistForEachVariable( "Eta" );
@@ -15,9 +15,8 @@ void DrawHistForEachVariable(TString var, Bool_t setZoomIn = kFALSE)
 {
 
   TString filebasedir = "hists/";
-
-  TString fileName1 = "L2overL1_NoL2PtCut__Run2016.root";
-  TString fileName2 = "L2overL1_NoL2PtCut__Run2018.root";
+  TString fileName1 = "2019_03_27_071841__L3overL1__Run2016.root";
+  TString fileName2 = "2019_03_27_071847__L3overL1__Run2018.root";
 
   TGraphAsymmErrors* g1 = GetEffGraph(filebasedir+fileName1, var);
   TGraphAsymmErrors* g2 = GetEffGraph(filebasedir+fileName2, var);
@@ -28,41 +27,37 @@ void DrawHistForEachVariable(TString var, Bool_t setZoomIn = kFALSE)
   PlotTool::GraphCanvaswRatio *canvasRatio = new PlotTool::GraphCanvaswRatio(canvasName, 0, 0);
 
   //==== output info
-  TString outdir = "outputs/L2overL1";
+  TString outdir = "outputs/L3overL1";
   gSystem->mkdir(outdir, kTRUE);
   canvasRatio->SetOutputDir(outdir);
 
-  canvasRatio->Register(g1, "2016 Data", kBlack);
-  canvasRatio->Register(g2, "2018 Data", kBlue);
+  canvasRatio->Register(g1, "Run2016H", kBlack);
+  canvasRatio->Register(g2, "Run2018D", kBlue);
 
   TString titleX = "";
-  if( var == "Pt" )  titleX = "p_{T}(#mu) [GeV]";
+  if( var == "Pt" )  titleX = "P_{T}(#mu) [GeV]";
   if( var == "Eta" ) titleX = "#eta(#mu)";
   if( var == "Phi" ) titleX = "#phi(#mu)";
-  if( var == "Vtx" ) titleX = "Number of primary vertices";
+  if( var == "Vtx" ) titleX = "# vtx";
 
-  canvasRatio->SetTitle( titleX, "L2 / L1 efficiency", "2018/2016");
+  canvasRatio->SetTitle( titleX, "Efficiency", "Run2018D/Run2016H");
   canvasRatio->SetLegendPosition( 0.60, 0.32, 0.95, 0.45 );
   // canvasRatio->SetLegendColumn(2);
 
-  if( var == "Pt" && setZoomIn )  canvasRatio->SetRangeX(24, 120);
-  if( var == "Pt" && !setZoomIn ) canvasRatio->SetRangeX(0, 500);
-  if( var == "Vtx" )              canvasRatio->SetRangeX(2.5, 50.5);
+  if( var == "Pt" && setZoomIn ) canvasRatio->SetRangeX(24, 120);
 
-  canvasRatio->SetRangeY( 0.90, 1.05 );
-  if( var == "Pt" && !setZoomIn) canvasRatio->SetRangeY( 0.90, 1.05 );
+  canvasRatio->SetRangeY( 0.8, 1.05 );
+  if( var == "Pt" && !setZoomIn) canvasRatio->SetRangeY( 0, 1.1 );
 
   canvasRatio->SetRangeRatio( 0.85, 1.15 );
 
   canvasRatio->Latex_CMSPre();
-  canvasRatio->RegisterLatex( 0.57, 0.96, "#font[42]{#scale[0.6]{10 fb^{-1} (13 TeV, 2016 and 2018)}}");
-                                        5 fb^{-1} (13 TeV, 2016), 5 fb^{-1} (13 TeV, 2018)
 
 
   //canvasRatio->RegisterLatex( 0.60, 0.96, "#font[42]{#scale[0.6]{RunXXX (XX pb^{-1})}}");
-  // canvasRatio->RegisterLatex( 0.16, 0.91, "#font[42]{#scale[0.6]{L2/L1}}");
+  canvasRatio->RegisterLatex( 0.16, 0.91, "#font[42]{#scale[0.6]{L3/L1}}");
   if( var != "Pt" )
-    canvasRatio->RegisterLatex( 0.16, 0.91, "#font[42]{#scale[0.6]{P_{T} > 26 GeV}}");
+    canvasRatio->RegisterLatex( 0.16, 0.87, "#font[42]{#scale[0.6]{P_{T} > 26 GeV}}");
 
   canvasRatio->Draw();
 }
